cbcc69a5a67da8be520a75da583aae799aeb4100
HandsomeYingyan <handsomeyingyan@gmail.com>
2021-10-08 08:07:32 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -10,8 +10,8 @@ breakfast $DEVICE
 make -j$(nproc) mkbootimg
 #make -j$(nproc) fec
 make -j$(nproc) halium-boot
-make -j$(nproc) systemimage 
+#make -j$(nproc) systemimage 
 
 echo "md5sum halium-boot.img and system.img"
 md5sum $ANDROID_ROOT/out/target/product/clover/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/clover/system.img
+#md5sum $ANDROID_ROOT/out/target/product/clover/system.img
