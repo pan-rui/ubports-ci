0559c6dbd8fe93b55352457bac8372e3c3638463
HandsomeYingyan <handsomeyingyan@github.com>
2021-10-10 15:59:50 +0800

    free up more space and add vendor

--- build-hal.sh
+++ build-hal.sh
@@ -8,10 +8,12 @@ bash hybris-patches/apply-patches.sh --mb
 export USE_CCACHE=1
 breakfast $DEVICE
 make -j$(nproc) mkbootimg
-#make -j$(nproc) fec
+make -j$(nproc) fec
 make -j$(nproc) halium-boot
-#make -j$(nproc) systemimage 
+make -j$(nproc) systemimage 
+make -j$(nproc) vendorimage
 
-echo "md5sum halium-boot.img and system.img"
+echo "md5sum halium-boot.img system.img vendor.img"
 md5sum $ANDROID_ROOT/out/target/product/clover/halium-boot.img
-#md5sum $ANDROID_ROOT/out/target/product/clover/system.img
+md5sum $ANDROID_ROOT/out/target/product/clover/system.img
+md5sum $ANDROID_ROOT/out/target/product/clover/vendor.img
